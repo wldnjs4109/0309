fece5231488f71db7066a7ba0bb011ff7a7abb9c
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-03-09 16:25:35 +0900

    Update and rename txt1.txt to 10171.java

---
+++ 10171.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
