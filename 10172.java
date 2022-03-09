999997b503035b53bc27707ea95db1a83d3d9707
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-03-09 16:28:05 +0900

    Create 10172.java

---
+++ 10172.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
